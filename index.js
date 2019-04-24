f32af1ce342ee30fd670c814c0e8ca9bcf36f232
Jason Kurian <jasonk92@gmail.com>
2019-04-24 12:20:49 -0400

    fix: incorrect logic, add test groups

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function xor(...args) {
+  if (args.length < 2) {
+    return false;
+  }
+
   const truthyCount = args.reduce((result, val) => result + !!val);
-  return 0 < truthyCount && truthyCount <= (args.length - 1);
+  return truthyCount === 1;
 };
--- index.test.js
+++ index.test.js
@@ -1,11 +1,21 @@
 const xor = require('.');
 
-test('examples', () => {
+test('not enough arguments', () => {
   expect(xor(true)).toBe(false);
   expect(xor(false)).toBe(false);
+});
+
+test('2 args', () => {
+  expect(xor(true, false)).toBe(true);
   expect(xor(false, true)).toBe(true);
+  expect(xor(true, true)).toBe(false);
+  expect(xor(false, false)).toBe(false);
+});
+
+test('many arguments', () => {
   expect(xor(true, false, false)).toBe(true);
   expect(xor(true, true, true)).toBe(false);
+  expect(xor(true, true, false)).toBe(false);
   expect(xor(false, false, false)).toBe(false);
   expect(xor(false, false, false, false, false, true)).toBe(true);
 });
